2493eecff59fbe832c02ddc93fc90b1d8752b7dc
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-08-25 02:04:25 +0000

    chore: automatically update REUSE headers

--- Content.IntegrationTests/Tests/GravityGridTest.cs
+++ Content.IntegrationTests/Tests/GravityGridTest.cs
@@ -18,6 +18,8 @@
 // SPDX-FileCopyrightText: 2024 Leon Friedrich
 // SPDX-FileCopyrightText: 2024 Tayrtahn
 // SPDX-FileCopyrightText: 2025 Coenx-flex
+// SPDX-FileCopyrightText: 2025 Cojoke
+// SPDX-FileCopyrightText: 2025 Redrover1760
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
